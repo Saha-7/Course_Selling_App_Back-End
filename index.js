740150130c488b45216ad923fbb3e2b48dcb6360
saha-7op <saha7pritam@gmail.com>
2024-05-23 13:34:54 +0530

    port update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
 
-const PORT = 3000;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
